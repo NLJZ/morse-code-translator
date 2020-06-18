bc3b1cc8cd9bb063180d9754967b8c67559a62ef
NLJZ <johnson.nathan.lee@gmail.com>
2020-06-18 20:34:31 +0200

    Updates

---
+++ precache-manifest.a5a56dfe642560f2e9d5296be3c8a0d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "523b3fba0f4e51ff8714641eae680247",
+    "url": "/morse-code-translator/index.html"
+  },
+  {
+    "revision": "b5fd7667ee3e797c71a4",
+    "url": "/morse-code-translator/static/css/main.f218eec1.chunk.css"
+  },
+  {
+    "revision": "48904a5948b567ebcba9",
+    "url": "/morse-code-translator/static/js/2.13951a71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/morse-code-translator/static/js/2.13951a71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5fd7667ee3e797c71a4",
+    "url": "/morse-code-translator/static/js/main.eb4f6c1b.chunk.js"
+  },
+  {
+    "revision": "c3b355d252cea5039005",
+    "url": "/morse-code-translator/static/js/runtime-main.3ddb7c11.js"
+  }
+]);
